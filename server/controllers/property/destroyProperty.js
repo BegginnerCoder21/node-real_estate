4461620d5252e8a2d68c03084ec60ec043251075
Teguera <aboubacarteguera21@example.com>
2024-02-09 01:30:31 +0000

    suppression d'une propriété implementé.

---
+++ server/controllers/property/destroyProperty.js
@@ -0,0 +1,23 @@
+const PropertyModel = require('../../models/Property');
+
+const destroyProperty = async(req, res) => {
+    try {
+
+        const property = await PropertyModel.findById(req.params.id);
+
+        if(property.currentOwner !== req.user.id) {
+
+            return res.status(403).json({"Unauthorized" : "Accès non authorisé à la suppression."});
+        }
+
+        await property.delete();
+
+        return res.status(200).json({"Success" : "Propriété supprimé avec succès."})
+        
+    } catch (error) {
+        
+        return res.status(500).json(error.message);
+    }
+}
+
+module.exports = destroyProperty;
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ const getProperty = require('../controllers/property/getProperty');
 const createNewProperty = require('../controllers/property/createNewProperty');
 const verifyToken = require('../middlewares/verifyToken');
 const updateProperty = require('../controllers/property/updateProperty');
+const destroyProperty = require('../controllers/property/destroyProperty');
 const router = express.Router()
 
 router.route('/register').post(register);
@@ -29,6 +30,6 @@ router.route('/create-property').post(verifyToken, createNewProperty);
 
 router.route('/update-property').put(verifyToken, updateProperty);
 
-
+router.route('/destroy-property').delete(verifyToken, destroyProperty);
 
 module.exports = router;
