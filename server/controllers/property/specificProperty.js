55ca1d722495feecd92faf3efabcc0b75be060dc
Teguera <aboubacarteguera21@example.com>
2024-02-09 00:13:39 +0000

    implementation de la recuperation d'une propriété specifique.

---
+++ server/controllers/property/specificProperty.js
@@ -0,0 +1,23 @@
+const PropertyModel = require("../../models/Property");
+
+const specificProperty = async(req, res) => {
+        const typeProperty = req.query;
+
+    try {
+        if(typeProperty){
+            const properties = await PropertyModel
+                                                .find(typeProperty)
+                                                .populate("currentOwner","-password");
+
+            return res.status(200).json(properties);
+        }else{
+
+            return res.status(500).json({"NotFound" : "type de propriété non trouvé"});
+        }
+        
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+module.exports = specificProperty;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const register = require('../controllers/authentication/register');
 const login = require('../controllers/authentication/login');
 const getProperty = require('../controllers/property/getProperty');
 const featuredProperty = require('../controllers/property/FeaturedProperty');
+const specificProperty = require('../controllers/property/specificProperty');
 const router = express.Router()
 
 router.route('/register').post(register);
@@ -12,5 +13,7 @@ router.route('/login').post(login);
 router.route('/all-properties').get(getProperty);
 
 router.route('/find/featured-properties').get(featuredProperty);
+
+router.route('/find').get(specificProperty);
 
 module.exports = router;
